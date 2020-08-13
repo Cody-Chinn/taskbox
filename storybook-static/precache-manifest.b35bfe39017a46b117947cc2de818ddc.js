59da9e47b7a6dfe32a459e85d5e9a002588f265d
Cody Chinn <cchinn1@steelcase.com>
2020-08-13 16:42:16 -0400

    taskbox UI

---
+++ storybook-static/precache-manifest.b35bfe39017a46b117947cc2de818ddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa3328d7f0cc6d6e6e0abebf4e2a3ff",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a59bdf964d9ca1a81ad9.bundle.js"
+  },
+  {
+    "url": "runtime~main.a59bdf964d9ca1a81ad9.bundle.js"
+  },
+  {
+    "revision": "79dcfb581913bdc0f7d1",
+    "url": "static/css/main.dc47b671.chunk.css"
+  },
+  {
+    "url": "vendors~main.a59bdf964d9ca1a81ad9.bundle.js"
+  },
+  {
+    "url": "vendors~main.a59bdf964d9ca1a81ad9.bundle.js.LICENSE.txt"
+  }
+]);
